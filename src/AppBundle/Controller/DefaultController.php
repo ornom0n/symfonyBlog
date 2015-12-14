8bfd21f0f62809d45a865a5598127800402f5e19
ornom0n <ornom0n@hotmail.com>
2015-12-14 21:45:01 +0200

    Added possiblity to add blog posts to database

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,10 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\BlogPost;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -13,9 +17,52 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        // $blogPost = new BlogPost();
+        // $blogPost->setTitle('HOT NEW SUSHIBAR');
+        // $blogPost->setUser('Bob Bobson');
+        // $blogPost->setMessage('I LOVE SUSHEY');
+
+        // $blogPost->setDate( new \DateTime('now') );
+
+        // $em = $this->getDoctrine()->getManager();
+
+        // $em->persist($blogPost);
+        // $em->flush();
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+
+    /**
+     * @Route("/add-blog-post", name="add_blog_post")
+     */
+    public function addBlogPostAction(Request $request){
+        $newPost = new BlogPost();
+        
+        $form = $this->createFormBuilder($newPost)
+            ->add( 'title', TextType::class )
+            ->add( 'user', TextType::class )
+            ->add( 'message', TextType::class )
+            ->add( 'save', SubmitType::class, array( 'label' => 'Create post!' ) )
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if( $form->isSubmitted()  && $form->isValid() ){
+            $newPost = $form->getData();
+            $newPost->setDate( new \DateTime('now') );
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newPost);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('default/add-blog-post.html.twig', array(
+            'form' => $form->createView(),
         ));
     }
 }
