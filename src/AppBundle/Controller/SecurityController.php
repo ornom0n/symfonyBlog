0a803bcfd8bf783b63515c2eea4de170495b17a2
ornom0n <ornom0n@hotmail.com>
2015-12-16 21:15:14 +0200

    procfile for Apache and PHP

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,22 @@
+<?php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login_route")
+     */
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+    }
+}
